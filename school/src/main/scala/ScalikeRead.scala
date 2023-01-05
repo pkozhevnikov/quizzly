a653357e7ab83ee5011f1b0edc7f4cb4b9f69f06
pkozhevnikov <techservio@gmail.com>
2023-01-05 15:55:21 +0500

    read setup

---
+++ school/src/main/scala/ScalikeRead.scala
@@ -0,0 +1,71 @@
+package quizzly.school
+
+import scalikejdbc.*
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+
+import java.time.ZoneId
+
+class ScalikeRead(poolName: String) extends Read:
+
+  case class Row(
+      id: String,
+      title: String,
+      status: String,
+      obsolete: Boolean,
+      role: Int,
+      person_id: String,
+      name: String
+  )
+
+  def quizList()(using ExecutionContext) = Future {
+    NamedDB(poolName).readOnly { implicit session =>
+      sql"select * from quizfact"
+        .map { rs =>
+          QuizListed(
+            rs.string("id"),
+            rs.string("title"),
+            rs.boolean("obsolete"),
+            rs.boolean("in_use"),
+            rs.boolean("is_published"),
+            rs.boolean("ever_published")
+          )
+        }
+        .list
+        .apply()
+    }
+  }
+
+  def examList()(using ExecutionContext) = Future {
+    NamedDB(poolName).readOnly { implicit session =>
+      sql"select * from exam"
+        .map { rs =>
+          ExamView(
+            rs.string("id"),
+            QuizRef(rs.string("quiz_id"), rs.string("quiz_title")),
+            ExamPeriod(
+              rs.zonedDateTime("start_at").withZoneSameInstant(ZoneId.of("Z")),
+              rs.zonedDateTime("end_at").withZoneSameInstant(ZoneId.of("Z"))
+            ),
+            Official(rs.string("host_id"), rs.string("host_name")),
+            rs.string("state"),
+            rs.zonedDateTimeOpt("cancelled_at").map(_.withZoneSameInstant(ZoneId.of("Z")).toInstant),
+            rs.int("trial_length"),
+            rs.zonedDateTime("prestart_at").withZoneSameInstant(ZoneId.of("Z"))
+          )
+        }
+        .list
+        .apply()
+    }
+  }
+
+  def testees(id: ExamID)(using ExecutionContext) = Future {
+    NamedDB(poolName).readOnly { implicit session =>
+      sql"select * from testee where exam_id=?".bind(id)
+        .map(rs => Person.of(rs.string("testee_place"))(rs.string("testee_id"), rs.string("testee_name")))
+        .list
+        .apply()
+    }
+  }
+
---
+++ school/src/test/scala/ScalikeReadSpec.scala
@@ -0,0 +1,92 @@
+package quizzly.author
+
+import akka.actor.testkit.typed.scaladsl.ActorTestKit
+import org.scalatest.*
+import scalikejdbc.*
+
+object ScalikeReadSpec:
+  import com.typesafe.config.ConfigFactory
+  val config =
+    ConfigFactory
+      .parseString("""
+    jdbc-connection-settings {
+      connection-pool {
+        timeout = 250ms
+        max-pool-size = 10
+      }
+      driver = "org.h2.Driver"
+      user = "sa"
+      password = "sa"
+      url = "jdbc:h2:mem:read"
+      migrations-table = "schemahistory"
+      migrations-locations = ["classpath:db"]
+      migration = on
+    }
+  """).resolve
+
+class ScalikeReadSpec extends wordspec.AsyncWordSpec, BeforeAndAfterAll, matchers.should.Matchers:
+
+  val testKit = ActorTestKit("readtest", ScalikeReadSpec.config)
+
+  import testdata.*
+
+  val q1 = QuizListed("1", "quiz 1", false, curator, authors, inspectors, Quiz.State.Composing)
+  val q2 = QuizListed(
+    "2",
+    "quiz 2",
+    false,
+    curator,
+    authors - author1,
+    inspectors + inspector3,
+    Quiz.State.Review
+  )
+  val q3 = QuizListed(
+    "3",
+    "quiz 3",
+    true,
+    author3,
+    authors + curator,
+    inspectors - inspector2,
+    Quiz.State.Released
+  )
+
+  def DB = NamedDB(testKit.system.name)
+
+  override def beforeAll() =
+    super.beforeAll()
+    ScalikeJdbcSetup(testKit.system)
+
+    NamedDB(testKit.system.name).localTx { implicit session =>
+      val insmemb = "insert into member (id,role,person_id,name) values (?,?,?,?)"
+      def ins(q: QuizListed) =
+        sql"insert into quiz (id,title,status,obsolete) values (?,?,?,?)"
+          .bind(q.id, q.title, q.state.toString, q.obsolete)
+          .update
+          .apply()
+        SQL(insmemb).bind(q.id, 1, q.curator.id, q.curator.name).update.apply()
+        q.authors
+          .foreach { a =>
+            SQL(insmemb).bind(q.id, 2, a.id, a.name).update.apply()
+          }
+        q.inspectors
+          .foreach { i =>
+            SQL(insmemb).bind(q.id, 3, i.id, i.name).update.apply()
+          }
+      ins(q1)
+      ins(q2)
+      ins(q3)
+    }
+
+  override def afterAll() =
+    super.afterAll()
+    testKit.shutdownTestKit()
+
+  "read side" should {
+    "return list of quizzes" in {
+      ScalikeRead(testKit.system.name)
+        .getList()
+        .map { list =>
+          list should contain inOrder (q1, q2, q3)
+        }
+    }
+  }
