88fffe032508800a235098ebbec5857f4f70fc1f
pkozhevnikov <techservio@gmail.com>
2023-01-12 15:00:53 +0500

    added persons state

---
+++ clients/school/src/app/persons.state.spec.ts
@@ -0,0 +1,49 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
+import { HttpHeaders } from "@angular/common/http"
+import { TestBed } from '@angular/core/testing'
+import { GlobalConfig } from "./global.config"
+import { UiStore } from "./ui.store"
+import { SessionQuery } from "./session/state/session.query"
+import { SessionStore } from "./session/state/session.store"
+import { PersonsState } from "./persons.state"
+
+describe('PersonsState', () => {
+  let state: PersonsState
+
+  let controller: HttpTestingController
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        UiStore, SessionQuery, SessionStore, PersonsState,
+        {provide: GlobalConfig, useValue: {baseApiUrl: "apiroot"}}
+      ],
+      imports: [ HttpClientTestingModule ]
+    })
+
+    state = TestBed.inject(PersonsState)
+    controller = TestBed.inject(HttpTestingController)
+    TestBed.inject(SessionStore).update({id: "user1", name: null})
+  })
+
+  afterEach(() => {
+    controller.verify()
+  })
+
+  it ("loads person list", done => {
+    state.get()
+    const req = controller.expectOne("apiroot/persons")
+    expect(req.request.method).toEqual("GET")
+    const data = [
+      {id: "off1", name: "off1 name", place: "Official"},
+      {id: "stud1", name: "stud1 name", place: "Student"}
+    ]
+    req.flush(data)
+    state.query.selectAll().subscribe(l => {
+      expect(l).toEqual(data)
+      done()
+    })
+  })
+
+})
+
---
+++ clients/school/src/app/persons.state.ts
@@ -0,0 +1,41 @@
+import { Injectable } from "@angular/core"
+import { EntityState, EntityStore, QueryEntity, StoreConfig } from "@datorama/akita"
+import { HttpBasedService } from "./util/httpbased.service"
+import { UiStore } from "./ui.store"
+import { GlobalConfig } from "./global.config"
+import { HttpClient } from "@angular/common/http"
+import { SessionQuery } from "./session/state/session.query"
+
+export interface Person extends EntityState<Person, string> {
+  id: string
+  name: string
+  place: string
+}
+@StoreConfig({name: "persons", idKey: "id"})
+class PersonsStore extends EntityStore<Person> {
+  constructor() {
+    super()
+  }
+}
+
+@Injectable({providedIn: "root"})
+export class PersonsState extends HttpBasedService {
+  
+  private store = new PersonsStore()
+  query = new QueryEntity<Person>(this.store)
+  constructor(
+    config: GlobalConfig,
+    http: HttpClient,
+    sessionQuery: SessionQuery,
+    uiStore: UiStore
+  ) {
+    super(config.baseApiUrl, http, sessionQuery, uiStore)
+  }
+
+  get() {
+    this.request(this.GET, "persons", {200: l => this.store.set(l)})
+  }
+}
+  
+
+
