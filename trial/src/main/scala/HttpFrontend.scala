052f7b120550cffc62fedfef0b88a70f6d074492
pkozhevnikov <techservio@gmail.com>
2023-01-27 15:11:20 +0500

    accept test setup

---
+++ trial/src/main/scala/HttpFrontend.scala
@@ -0,0 +1,154 @@
+package quizzly.trial
+
+import akka.actor.typed.ActorRef
+import akka.actor.typed.ActorSystem
+import akka.cluster.sharding.typed.scaladsl.EntityRef
+import akka.http.scaladsl.common.EntityStreamingSupport
+import akka.http.scaladsl.common.JsonEntityStreamingSupport
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import akka.http.scaladsl.marshalling.ToResponseMarshaller
+import akka.http.scaladsl.marshalling.ToEntityMarshaller
+import akka.http.scaladsl.model.*
+import akka.http.scaladsl.server.Directives.*
+import akka.http.scaladsl.server.RequestContext
+import akka.http.scaladsl.server.Route
+import akka.stream.scaladsl.Source
+import akka.util.ByteString
+import spray.json.*
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+import scala.concurrent.duration.*
+import scala.util.Failure
+import scala.util.Success
+import scala.util.Try
+
+import java.time.*
+
+import ch.megard.akka.http.cors.scaladsl.CorsDirectives.*
+
+case class ExamInfo(
+  quizId: QuizID,
+  quizTitle: String,
+  quizIntro: String,
+  id: ExamID,
+  start: Instant,
+  end: Instant,
+  trialLength: Int
+)
+case class Solution(
+  itemSC: SC,
+  answers: List[String]
+)
+
+
+trait JsonFormats extends SprayJsonSupport, DefaultJsonProtocol:
+  given RootJsonFormat[ExamInfo] = jsonFormat7(ExamInfo.apply)
+  given RootJsonFormat[Reason] = jsonFormat2(Reason.apply)
+  given RootJsonFormat[Error] = jsonFormat2(Error.apply)
+  given RootJsonFormat[Person] = jsonFormat2(Person.apply)
+  given RootJsonFormat[Instant] =
+    new:
+      def write(i: Instant) = JsString(i.toString)
+      def read(n: JsValue) =
+        n match
+          case JsString(str) =>
+            Instant.parse(str)
+          case x =>
+            throw DeserializationException(s"unknown value for instant $x")
+  given RootJsonFormat[ZonedDateTime] =
+    new:
+      def write(zdt: ZonedDateTime) = JsString(zdt.toString)
+      def read(n: JsValue) =
+        n match
+          case JsString(str) =>
+            ZonedDateTime.parse(str)
+          case x =>
+            throw DeserializationException(s"unknown value for zoned datetime $x")
+  given RootJsonFormat[Statement] = jsonFormat2(Statement.apply)
+  given RootJsonFormat[ItemView] = jsonFormat5(ItemView.apply)
+  given RootJsonFormat[SectionView] = jsonFormat3(SectionView.apply)
+  given RootJsonFormat[Solution] = jsonFormat2(Solution.apply)
+  given RootJsonFormat[SubmissionResult] = jsonFormat2(SubmissionResult.apply)
+
+  given JsonEntityStreamingSupport = EntityStreamingSupport.json()
+
+trait EntityAware:
+  def exam(id: ExamID): EntityRef[ExamEntity.Command]
+  def trial(id: TrialID): EntityRef[Trial.Command]
+
+trait Auth:
+  def authenticate(request: HttpRequest): Future[Person]
+
+object HttpFrontend extends JsonFormats:
+
+  val log = org.slf4j.LoggerFactory.getLogger("HttpFrontend")
+
+  def apply(
+      entities: EntityAware,
+      authService: Auth,
+      host: String = "localhost",
+      port: Int = 9099
+  )(using sys: ActorSystem[?], ec: ExecutionContext, now: () => Instant) =
+
+    def auth(request: HttpRequest)(next: Person => Route) =
+      onComplete(authService.authenticate(request)) {
+        case Success(p) =>
+          next(p)
+        case Failure(ex) =>
+          complete(StatusCodes.Unauthorized)
+      }
+
+    given akka.util.Timeout = 2.seconds
+
+    def call[R, Cmd](
+        entity: EntityRef[Cmd]
+    )(cmd: ActorRef[Resp[R]] => Cmd)(using ToEntityMarshaller[R]) =
+      onComplete(entity.ask(cmd)) {
+        case Success(Resp.OK) =>
+          complete(StatusCodes.NoContent)
+        case Success(Resp.Good(r)) =>
+          complete(r)
+        case Success(Resp.Bad(e)) =>
+          complete(StatusCodes.UnprocessableEntity, e)
+        case Failure(ex) =>
+          complete(StatusCodes.InternalServerError, ex.getMessage)
+      }
+
+    def onTrial[R](id: TrialID)(cmd: ActorRef[Resp[R]] => Trial.Command)(using ToEntityMarshaller[R]) =
+      call[R, Trial.Command](entities.trial(id))(cmd)
+
+    def onExam[R](id: ExamID)(cmd: ActorRef[Resp[R]] => ExamEntity.Command)(using ToEntityMarshaller[R]) =
+      call[R, ExamEntity.Command](entities.exam(id))(cmd)
+
+    // format: off
+    cors() {
+      pathPrefix("pubapi")(pubapi(host, port)) ~
+      extractRequest { request =>
+        auth(request) { person =>
+          pathPrefix("v1") {
+            complete("trial module works")
+          }
+        }
+      }
+    }
+    // format: on
+
+  val yamlContentType = ContentType(
+    MediaType.textWithFixedCharset("vnd.yaml", HttpCharsets.`UTF-8`, "yaml", "yml")
+  )
+  private def pubapi(host: String, port: Int) = concat(
+    pathSingleSlash(getFromResource("pubapi/index.html")),
+    path("root.yaml") {
+      complete(
+        HttpEntity(
+          yamlContentType,
+          Source
+            .fromIterator(() => scala.io.Source.fromResource("pubapi/root.yaml").getLines())
+            .map(_.replace("""${http.host}""", host).replace("""${http.port}""", port.toString))
+            .map(l => ByteString(s"$l\n"))
+        )
+      )
+    },
+    getFromResourceDirectory("pubapi")
+  )
---
+++ trial/src/test/scala/TrialSessionSpec.scala
@@ -0,0 +1,173 @@
+package quizzly.trial
+
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.client.RequestBuilding.*
+import akka.http.scaladsl.marshalling.*
+import akka.http.scaladsl.model.*
+import akka.http.scaladsl.unmarshalling.*
+import com.typesafe.config.ConfigFactory
+import org.scalatest.*
+
+import scala.concurrent.Await
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+import scala.concurrent.duration.*
+
+import java.time.*
+
+object TrialSessionSpec:
+  def config(nodePort: Int, httpPort: Int) =
+    ConfigFactory
+      .parseString(s"""
+    akka.cluster.seed-nodes = ["akka://app@localhost:$nodePort"]
+    akka.remote.artery.canonical.port = "$nodePort"
+    jdbc-connection-settings.url = "jdbc:h2:mem:trialtest"
+    frontend.http.port = $httpPort
+    """)
+      .withFallback(ConfigFactory.load("application.conf"))
+      .resolve
+
+class TrialSessionSpec
+    extends featurespec.AnyFeatureSpec,
+      GivenWhenThen,
+      BeforeAndAfterAll,
+      concurrent.Eventually,
+      concurrent.IntegrationPatience,
+      JsonFormats,
+      matchers.should.Matchers:
+
+  import scala.util.Using
+  import java.net.ServerSocket
+  def freePort(default: Int) = Using(new ServerSocket(0))(_.getLocalPort).getOrElse(default)
+  val httpPort = freePort(33035)
+  val nodePort = freePort(44046)
+
+  val appSystem = ActorSystem(Behaviors.empty, "trialapp", TrialSessionSpec.config(nodePort, httpPort))
+  val clnSystem = ActorSystem(
+    Behaviors.empty,
+    "trialcln",
+    ConfigFactory.parseString("""{akka.actor.provider = "local"}""")
+  )
+
+  given ActorSystem[?] = clnSystem
+  given ExecutionContext = clnSystem.executionContext
+  given (() => Instant) = () => Instant.parse("2022-11-01T00:00:00Z")
+
+  val http = Http()(using clnSystem)
+
+  extension (response: HttpResponse)
+    def to[C](using FromResponseUnmarshaller[C]) = Await.result(Unmarshal(response).to[C], 1.second)
+  extension (path: String)
+    def fullUrl = s"http://localhost:$httpPort/v1/$path"
+
+  given ToEntityMarshaller[Set[String]] = sprayJsonMarshaller[Set[String]]
+  def request(req: HttpRequest, as: Person) = Await
+    .result(http.singleRequest(req ~> addHeader("p", as.id)), 3.second)
+  def post[C](path: String, as: Person, content: C)(using ToEntityMarshaller[C]) = request(
+    Post(path.fullUrl, content),
+    as
+  )
+  def patch(path: String, as: Person) = request(Patch(path.fullUrl), as)
+  def get(path: String, as: Person) = request(Get(path.fullUrl), as)
+
+  val pers1 = Person("pers1", "pers1 name")
+  val pers2 = Person("pers2", "pers2 name")
+  val pers3 = Person("pers3", "pers3 name")
+  val all = Map(
+    pers1.id -> pers1,
+    pers2.id -> pers2,
+    pers3.id -> pers3
+  )
+
+  val item1 = ItemView("i1", "i1 intro", Statement("i1 def", None),
+    List(Statement("i1 h1", None), Statement("i1 h2", None)), false)
+  val item2 = ItemView("i2", "i2 intro", Statement("i2 def", None),
+    List(Statement("i2 h1", None), Statement("i2 h2", None)), true)
+  val item3 = ItemView("i3", "i3 intro", Statement("i3 def", None),
+    List(Statement("i3 h1", None), Statement("i3 h2", None)), false)
+  val section1 = SectionView("section1 title", "section1 intro", List(item1, item2))
+  val section2 = SectionView("section2 title", "section2 intro", List(item3))
+
+  val auth: Auth =
+    new:
+      def authenticate(req: HttpRequest) =
+        all.get(req.getHeader("p").get.value) match
+          case Some(p: Person) =>
+            Future(p)
+          case _ =>
+            Future.failed(Exception())
+
+  //val getFact = Main(appSystem, auth)
+
+  override def beforeAll() =
+    super.beforeAll()
+
+  override def afterAll() =
+    super.afterAll()
+    clnSystem.terminate()
+    appSystem.terminate()
+
+  import Trial.*
+
+  info("Testee tries a quiz")
+
+  Feature("getting exam info") {
+
+    Scenario("exam info returned") {
+      When("exam info is requested")
+      val res = get("exam-1", pers1)
+      Then("exam info is returned")
+      res.status shouldBe StatusCodes.OK
+      res.to[ExamInfo] shouldBe ExamInfo(
+        "q1", "q1 title", "q1 intro",
+        "exam-1", Instant.parse("2023-01-28T10:00:00Z"), Instant.parse("2023-01-30T10:00:00Z"), 55
+      )
+    }
+
+    Scenario("error getting exam info") {
+      When("info of notexisting exam is reqeusted")
+      val res = get("nonexists", pers1)
+      Then("error returned")
+      res.status shouldBe StatusCodes.UnprocessableEntity
+      res.to[Error] shouldBe examNotFound.error()
+    }
+
+    Scenario("access denied for not authenticated user") {
+      When("exam info is requested by non-registered used")
+      val res = get("exam-1", Person("notexists", ""))
+      Then("access denied")
+      res.status shouldBe StatusCodes.Unauthorized
+    }
+
+  }
+
+  Feature("starting trial") {
+    
+    Scenario("not starting if exam not registered") {
+      When("start trial of non-existant exam requested")
+      val res = patch("exam-3", pers1)
+      Then("error returned")
+      res.status shouldBe StatusCodes.UnprocessableEntity
+      res.to[Error] shouldBe examNotFound.error()
+    }
+
+    Scenario("not starting by user not included in exam") {
+      When("start requested by not included testee")
+      val res = patch("exam-1", pers3)
+      Then("error returned")
+      res.status shouldBe StatusCodes.UnprocessableEntity
+      res.to[Error] shouldBe notTestee.error()
+    }
+
+    Scenario("successful start") {
+      When("start trial requested")
+      val res = patch("exam-1", pers1)
+      Then("trial started")
+      res.status shouldBe StatusCodes.OK
+      And("first quiz section returned")
+      res.to[SectionView] shouldBe section1
+    }
+
+  }
