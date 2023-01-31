5e2035bfb383bf0afd0801ce0ac21d2f458a6ced
pkozhevnikov <techservio@gmail.com>
2023-01-31 23:05:21 +0500

    school: added registry impl

--- school/src/main/scala/SchoolRegistryImpl.scala
+++ school/src/main/scala/SchoolRegistryImpl.scala
@@ -3,7 +3,12 @@ package quizzly.school
 import scala.concurrent.{ExecutionContext, Future}
 import akka.actor.typed.RecipientRef
 
-class SchoolRegistryImpl(facts: QuizID => RecipientRef[QuizFact.Command])(using ExecutionContext)
+import java.time.Instant
+
+class SchoolRegistryImpl(
+    facts: QuizID => RecipientRef[QuizFact.Command],
+    exams: ExamID => RecipientRef[Exam.Command]
+)(using ExecutionContext)
     extends grpc.SchoolRegistry:
 
   override def registerQuiz(in: grpc.RegisterQuizRequest): Future[grpc.RegisterQuizResponse] =
@@ -13,5 +18,17 @@ class SchoolRegistryImpl(facts: QuizID => RecipientRef[QuizFact.Command])(using 
     }
 
   override def registerTrialResults(
-      results: grpc.RegisterTrialResultsRequest
-  ): Future[grpc.RegisterTrialResultsResponse] = ???
+      in: grpc.RegisterTrialResultsRequest
+  ): Future[grpc.RegisterTrialResultsResponse] = Future {
+    exams(in.examId) !
+      Exam.RegisterTrial(
+        TrialOutcome(
+          in.testeeId,
+          in.trialId,
+          Instant.ofEpochSecond(in.start),
+          Instant.ofEpochSecond(in.end),
+          in.solutions.map(s => Solution(s.sectionSc, s.itemSc, s.answers.toList)).toList
+        )
+      )
+    grpc.RegisterTrialResultsResponse.of()
+  }
