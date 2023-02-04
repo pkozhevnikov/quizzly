af7e3965038e1d4698275ff4330eeac73166cf05
pkozhevnikov <techservio@gmail.com>
2023-02-04 17:48:46 +0500

    default grader tested

--- school/src/main/scala/Grader.scala
+++ school/src/main/scala/Grader.scala
@@ -17,27 +17,50 @@ class DefaultGrader extends Grader:
       sections: List[Section],
       trialDurationSeconds: Int,
       solutions: List[Solution]
-  ) = 
-    val allItems = sections.flatMap(section => section.items.map(item => {
-      solutions.find(sol => sol.sectionSc == section.sc && sol.itemSc == item.sc) match
-        case Some(solution) =>
-          gradeItem(item, solution.answers)
-        case None =>
-          0
-    }))
-    math.ceil(allItems.sum.toDouble / allItems.length).toInt
+  ) =
+    val allItems = sections.flatMap(section =>
+      section
+        .items
+        .map(item => {
+          solutions.find(sol => sol.sectionSc == section.sc && sol.itemSc == item.sc) match
+            case Some(solution) =>
+              gradeItem(item, solution.answers)
+            case None =>
+              0
+        })
+    )
+    math.ceil(allItems.sum / allItems.length).toInt
 
   val phr = "\\{\\{(\\d+)}}".r
 
-  def gradeItem(item: Item, answers: List[String]): Int =
-    val fillSolutions = phr.findAllMatchIn(item.definition.text).map(_.group(1)).toList
-    val (sol, ans) = 
+  def gradeItem(item: Item, answers: List[String]): Double =
+    val fillSolutions =
+      phr.findAllMatchIn(item.definition.text).map(_.group(1).toInt - 1).toList.distinct
+    def intAnswers =
+      answers
+        .map(v => Try(v.toInt))
+        .filter(_.isSuccess)
+        .map(_.get)
+        .filter(idx => idx > -1 && idx < item.hints.length)
+        .distinct
     if fillSolutions.isEmpty then
-      (item.solutions, answers.map(v => Try(v.toInt)).filter(_.isSuccess).map(_.get))
-    else
-      (fillSolutions.map(_.toInt), List.empty)
-
-    if ans.length != sol.length then
+      val ans = intAnswers
+      if ans.length != item.solutions.length then
+        0
+      else
+        ans.count(item.solutions.contains(_)).toDouble / item.solutions.length * 100
+    else if item.hintsVisible then
+      val ans = intAnswers
+      if ans.length != fillSolutions.length then
+        0
+      else
+        fillSolutions.zip(ans).count((s, a) => s == a).toDouble / fillSolutions.length * 100
+    else if answers.length != fillSolutions.length then
       0
     else
-      math.ceil(sol.zip(ans).count((s, a) => s == a).toDouble / sol.length * 100).toInt
+      fillSolutions
+        .zip(answers)
+        .count { (s, a) =>
+          item.hints(s).map(_.text.toLowerCase.trim).contains(a.toLowerCase.trim)
+        }
+        .toDouble / fillSolutions.length * 100
--- school/src/test/scala/DefaultGraderSpec.scala
+++ school/src/test/scala/DefaultGraderSpec.scala
@@ -4,39 +4,169 @@ import org.scalatest.*
 
 class DefaultGraderSpec extends wordspec.AnyWordSpec, matchers.should.Matchers, BeforeAndAfterEach:
 
-
-  def oneItem(hints: Int, hintsVisible: Boolean, solutions: List[Int]) =
-    List(Section("s1", "", "", List(Item("s1i1", "", Statement("", None),
-      List.fill(hints)(List.empty), hintsVisible, solutions))))
+  def oneItem(
+      hints: Int,
+      hintsVisible: Boolean,
+      solutions: List[Int],
+      definitionText: String = ""
+  ) = List(
+    Section(
+      "s1",
+      "",
+      "",
+      List(
+        Item(
+          "s1i1",
+          "",
+          Statement(definitionText, None),
+          List.fill(hints)(List.empty),
+          hintsVisible,
+          solutions
+        )
+      )
+    )
+  )
 
   var grader = DefaultGrader()
 
-  "DefaultGraderSpec" should {
+  val addword = afterWord("estimate")
 
-    "estimate correct sinlge choice item" in {
-      def sol(value: String) = List(Solution("s1", "s1i1", List(value)))
-      val sections = oneItem(hints = 5, hintsVisible = true, solutions = List(3))
-      grader.estimate(0, sections, 0, sol("3")) shouldBe 100
-      grader.estimate(0, sections, 0, sol("2")) shouldBe 0
-      grader.estimate(0, sections, 0, List.empty) shouldBe 0
+  "DefaultGrader" should addword {
+
+    "item" which {
+
+      "single choice" in {
+        def sol(value: String) = List(Solution("s1", "s1i1", List(value)))
+        val sections = oneItem(hints = 5, hintsVisible = true, solutions = List(3))
+        grader.estimate(0, sections, 0, sol("3")) shouldBe 100
+        grader.estimate(0, sections, 0, sol("2")) shouldBe 0
+        grader.estimate(0, sections, 0, List.empty) shouldBe 0
+      }
+
+      "multi choice" in {
+        val sections = oneItem(hints = 9, hintsVisible = true, solutions = List(1, 4, 7))
+        def estimate(value: Int*) = grader
+          .estimate(0, sections, 0, List(Solution("s1", "s1i1", value.toList.map(_.toString))))
+        estimate(1, 4, 7) shouldBe 100
+        estimate(1, 4, 3) shouldBe 67
+        estimate(1, 4) shouldBe 0
+        estimate(1, 2, 4, 7) shouldBe 0
+        estimate(1, 5, 7) shouldBe 67
+        estimate(0, 1, 5) shouldBe 34
+        estimate(2, 3, 7) shouldBe 34
+      }
+
+      "fill select" in {
+        val sections = oneItem(
+          hints = 9,
+          hintsVisible = true,
+          solutions = List.empty,
+          "text {{3}} text {{7}} text {{6}}"
+        )
+        def estimate(value: Int*) = grader
+          .estimate(0, sections, 0, List(Solution("s1", "s1i1", value.toList.map(_.toString))))
+        estimate(2, 6, 5) shouldBe 100
+        estimate(2, 6, 5, 0) shouldBe 0
+        estimate(2, 3, 5) shouldBe 67
+        estimate(6, 5, 7) shouldBe 0
+        estimate(2, 6, 7) shouldBe 67
+        estimate(1, 6, 3) shouldBe 34
+        estimate(1, 6, 5) shouldBe 67
+      }
+
+      "fill by hand" in {
+        val sections = List(
+          Section(
+            "s1",
+            "",
+            "",
+            List(
+              Item(
+                "s1i1",
+                "",
+                Statement("text {{1}} text {{5}} text {{6}}", None),
+                (1 to 8)
+                  .map(n =>
+                    List(Statement(s"hint$n ALT1", None), Statement(s"hint$n alt2", None))
+                  )
+                  .toList,
+                false,
+                List.empty
+              )
+            )
+          )
+        )
+
+        def estimate(value: String*) = 
+          grader.estimate(0, sections, 0, List(Solution("s1", "s1i1", value.toList)))
+
+        estimate("hint1 aLT1", "HINT5 alt2", "HINT6 ALT1") shouldBe 100
+        estimate("hint1 aLT1", "HINT5 alt2", "HINT6 ALT1", "x") shouldBe 0
+        estimate("", "", "") shouldBe 0
+        estimate("x", "hint1 alt1", "hint6 alt2") shouldBe 34
+        estimate("hint1 alt2", "y", "hint6 alt1") shouldBe 67
+      }
+
     }
 
-    "estimate trial correctly" ignore {
-      val sections = List(
-        Section("s1", "", "", List(
-          Item(
-            "s1i1",
-            "",
-            Statement("", None),
-            List.fill(5)(List.empty),
-            true,
-            List(0, 2)
-        )))
-      )
-      val grader = DefaultGrader()
-      //grader.estimate(0, sections, 0, List("1", "2")) shouldBe 50
-      //grader.estimate(0, sections, 0, List("0", "2")) shouldBe 100
-      //grader.estimate(0, sections, 0, List("0", "1", "2", "3", "4")) shouldBe 
+    "one section" which {
+
+      "contains mutiple items" in {
+        //format off
+        val sections = List(
+          Section(
+            "s1", "", "", List(
+              Item("s1i1", "", Statement("", None), List.fill(5)(List.empty), true, List(1,3)),
+              Item("s1i2", "", Statement("", None), List.fill(3)(List.empty), true, List(2)),
+              Item("s1i3", "", Statement("{{1}} {{3}}", None), List.fill(5)(List.empty), true, List.empty),
+              Item("s1i4", "", Statement("{{1}} {{3}}", None), List(
+                List(Statement("hint1", None)), List(Statement("hint2", None)),
+                List(Statement("hint3", None)), List(Statement("hint4", None))
+              ), false, List.empty)
+            )
+          )
+        )
+        grader.estimate(0, sections, 0, List(
+          Solution("s1", "s1i2", List("2")),
+          Solution("s1", "s1i1", List("1", "3")),
+          Solution("s1", "s1i3", List("0", "2")),
+          Solution("s1", "s1i4", List("hint1", "hint3"))
+        )) shouldBe 100
+        grader.estimate(0, sections, 0, List(
+          Solution("s1", "s1i1", List.empty),
+          Solution("s1", "s1i2", List("2")),
+          Solution("s1", "s1i3", List("0", "3")),
+          Solution("s1", "s1i4", List("Hint2", "hint3"))
+        )) shouldBe 50
+        //format on
+      }
+
+    }
+
+    "multiple sections" that {
+      "contains one item each" in {
+        //format off
+        val sections = List(
+          Section(
+            "s1", "", "", List(
+              Item("s1i1", "", Statement("", None), List.fill(6)(List.empty), true, List(1, 3, 4))
+            )
+          ),
+          Section(
+            "s2", "", "", List(
+              Item("s2i1", "", Statement("", None), List.fill(5)(List.empty), true, List(2))
+            )
+          )
+        )
+        grader.estimate(0, sections, 0, List(
+          Solution("s1", "s1i1", List("1", "2", "3")),
+          Solution("s2", "s2i1", List("2"))
+        )) shouldBe 84
+        grader.estimate(0, sections, 0, List(
+          Solution("s1", "s1i1", List("1", "2", "5"))
+        )) shouldBe 17
+        //fomat on
+      }
     }
 
   }
